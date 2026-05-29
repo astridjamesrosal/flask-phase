d294532d58b8cd1d885cf6a0aa78df41e481dee0
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-05-29 14:14:47 +0800

    topic1: forms and user input

--- topic1_fundamentals/app.py
+++ topic1_fundamentals/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "Astral"
-    return render_template('index.html', name=name)
+    return render_template('index.html')
+
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        name = request.form['username']
+        return render_template('greet.html', name=name)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
