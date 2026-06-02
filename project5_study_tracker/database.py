fb95a9e48ed2befa03ff12b2e845529c4e01bea2
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-06-02 12:54:08 +0800

    db: add session CRUD functions to database.py

--- project5_study_tracker/database.py
+++ project5_study_tracker/database.py
@@ -14,7 +14,28 @@ def init_db():
         )
         """)
     connection.commit()
+    connection.close()
 
+def add_session(subject, start_time, end_time, key_takeaways, productivity_rating):
+    connection = sqlite3.connect('study_sessions.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Sessions (subject, start_time, end_time, key_takeaways, productivity_rating) VALUES (?, ?, ?, ?, ?)", (subject, start_time, end_time, key_takeaways, productivity_rating))
+    connection.commit()
+    connection.close()
+
+def get_all_sessions():
+    connection = sqlite3.connect('study_sessions.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Sessions")
+    sessions = cursor.fetchall()
+    connection.close()
+    return sessions
+
+def delete_session(session_id,):
+    connection = sqlite3.connect('study_sessions.db')
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Sessions WHERE session_id = ?;", (session_id,))
+    connection.commit()
     connection.close()
 
 init_db()
