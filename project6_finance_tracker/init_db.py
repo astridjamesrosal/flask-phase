528e0c2e219e1f1580639aac5629097e74a0fbdf
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-06-07 21:18:23 +0800

    add init_db.py: create Accounts, Categories, Transactions tables

---
+++ project6_finance_tracker/init_db.py
@@ -0,0 +1,34 @@
+import sqlite3 
+
+def init_db():
+    connection = sqlite3.connect('finance_tracker.db')
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Accounts (
+            account_id INTEGER PRIMARY KEY,
+            name TEXT,
+            account_type TEXT,
+            is_active INTEGER DEFAULT 1
+        )
+        """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Categories (
+            category_id INTEGER PRIMARY KEY,
+            category_name TEXT,
+            is_active INTEGER DEFAULT 1
+        )    
+        """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Transactions (
+            transaction_id INTEGER PRIMARY KEY,
+            Date TEXT,
+            account_id INTEGER REFERENCES Accounts(account_id),
+            category_id INTEGER REFERENCES Categories(category_id),
+            transaction_type TEXT,
+            amount FLOAT 
+        )
+        """)
+    connection.commit()
+    connection.close()
+
+
