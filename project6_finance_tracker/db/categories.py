c86fbbd3bbb652cfb27297004c71299b1325c03b
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-06-25 12:01:36 +0800

    Change default category color to red on conflict-test

--- project6_finance_tracker/db/categories.py
+++ project6_finance_tracker/db/categories.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def create_category(category_name, color='#ffffff'):
+def create_category(category_name, color='#ff0000'):
     connection = sqlite3.connect('finance_tracker.db')
     connection.execute("PRAGMA foreign_keys = ON")
     cursor = connection.cursor()
