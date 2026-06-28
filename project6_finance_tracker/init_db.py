64c6246b57521bd915b4fb72973491a3fd6423a0
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-06-28 21:08:54 +0800

    Add NOT NULL constraints to database schema

--- project6_finance_tracker/init_db.py
+++ project6_finance_tracker/init_db.py
@@ -6,27 +6,27 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS Accounts (
             account_id INTEGER PRIMARY KEY,
-            name TEXT,
-            account_type TEXT,
-            is_active INTEGER DEFAULT 1
+            name TEXT NOT NULL,
+            account_type TEXT NOT NULL,
+            is_active INTEGER NOT NULL DEFAULT 1
         )
         """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS Categories (
             category_id INTEGER PRIMARY KEY,
-            category_name TEXT,
-            is_active INTEGER DEFAULT 1,
+            category_name TEXT NOT NULL,
+            is_active INTEGER NOT NULL DEFAULT 1,
                    color TEXT NOT NULL DEFAULT '#a78bfa'
         )    
         """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS Transactions (
             transaction_id INTEGER PRIMARY KEY,
-            date TEXT,
-            account_id INTEGER REFERENCES Accounts(account_id),
-            category_id INTEGER REFERENCES Categories(category_id),
-            transaction_type TEXT,
-            amount FLOAT,
+            date TEXT NOT NULL,
+            account_id INTEGER NOT NULL REFERENCES Accounts(account_id),
+            category_id INTEGER NOT NULL REFERENCES Categories(category_id),
+            transaction_type TEXT NOT NULL,
+            amount FLOAT NOT NULL,
             description TEXT
         )
         """)
